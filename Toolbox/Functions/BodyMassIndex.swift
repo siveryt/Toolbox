94ae92936f235cd8beb36f86b7782759e7ae7e62
Sivery <50551841+siveryt@users.noreply.github.com>
2022-09-05 17:59:06 +0200

    1.1.3 BMI Calculation works now

--- Toolbox/Functions/BodyMassIndex.swift
+++ Toolbox/Functions/BodyMassIndex.swift
@@ -13,11 +13,11 @@ struct BodyMassIndex: View {
     let metric = Locale.current.usesMetricSystem
     let defaults = UserDefaults.standard
     
-    @AppStorage("bmiHeight") var height = ""
-    @AppStorage("bmiWeight") var weight = ""
+    @State var height = ""
+    @State var weight = ""
     
-    @AppStorage("bmiH") var h:Double = 0
-    @AppStorage("bmiW") var w:Double = 0
+    @State var h:Double = 0
+    @State var w:Double = 0
     
     
     
@@ -119,6 +119,20 @@ struct BodyMassIndex: View {
         
         .navigationBarTitleDisplayMode(/*@START_MENU_TOKEN@*/.inline/*@END_MENU_TOKEN@*/)
         .navigationTitle("BMI")
+        .onAppear(){
+            if(defaults.valueExists(forKey: "bmiHeight")){
+                height = defaults.string(forKey: "bmiHeight")!
+            }
+            if(defaults.valueExists(forKey: "bmiWeight")){
+                weight = defaults.string(forKey: "bmiWeight")!
+            }
+            if(defaults.valueExists(forKey: "bmiHeightD")){
+                h = defaults.double(forKey: "bmiHeightD")
+            }
+            if(defaults.valueExists(forKey: "bmiWeightD")){
+                w = defaults.double(forKey: "bmiWeightD")
+            }
+        }
     
     }
     
