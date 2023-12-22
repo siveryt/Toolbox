863f5364c3d9a7d66d7b2337e0ba8906bea94602
Sivery <50551841+siveryt@users.noreply.github.com>
2023-12-22 22:32:00 +0100

    Live Clock bigger

--- Toolbox/Functions/LiveClock.swift
+++ Toolbox/Functions/LiveClock.swift
@@ -16,20 +16,33 @@ extension Date {
 }
 
 
+
+
 struct LiveClock: View {
     
     @State var now = Date()
     
     var body: some View {
-        Text(now.getFormattedDate(format: "HH:mm:ss"))
-            .onAppear(perform: {self.now = Date(); let _ = self.updateTimer})
-            .dynamicTypeSize(/*@START_MENU_TOKEN@*/.accessibility5/*@END_MENU_TOKEN@*/)
+        
+        GeometryReader { geometry in
+            Text(now.getFormattedDate(format: "HH:mm:ss"))
+                .font(.system(size: min(geometry.size.width, geometry.size.height) * 0.8, design: .default)).monospacedDigit()
+                .lineLimit(1)
+                .minimumScaleFactor(0.01)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding() // Adds default padding
+                .onAppear(perform: {self.now = Date(); let _ = self.updateTimer})
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        Text(now.getFormattedDate(format: "HH:mm:ss"))
+//            .onAppear(perform: {self.now = Date(); let _ = self.updateTimer})
+//            .font(.system(size: calculateFontSize()))
         .navigationBarTitleDisplayMode(/*@START_MENU_TOKEN@*/.inline/*@END_MENU_TOKEN@*/)
         .navigationTitle("Live Clock")
         
     }
     var updateTimer: Timer {
-        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true,
+        Timer.scheduledTimer(withTimeInterval: 0.001, repeats: true,
                              block: {_ in
             self.now = Date()
         })
