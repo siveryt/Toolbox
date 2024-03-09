ac5e62e41a14362aa67fd92398e4338d605e0ba9
Sivery <50551841+siveryt@users.noreply.github.com>
2024-03-09 16:32:23 +0100

    Added Widget Target for Dice

---
+++ diceWidget/diceWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  diceWidgetBundle.swift
+//  diceWidget
+//
+//  Created by Christian Nagel on 09.03.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct diceWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        diceWidget()
+    }
+}
